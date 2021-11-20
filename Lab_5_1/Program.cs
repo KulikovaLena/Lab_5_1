7d6219b670c3a63ce390938b434cfa0ada0dab55
11111 <11111@DESKTOP-I2P1BG6>
2021-11-21 01:49:08 +0300

    Add project files.

---
+++ Lab_5_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            float S = 0;
+            Console.WriteLine("Vvedite 7 celochislennih znacheniy");
+            for (int i = 0; i < 7; i++)
+            {
+                int n;
+                n = Convert.ToInt32(Console.ReadLine());
+                array[i] = n;
+                S += array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0:f2} - srednee arifmeticheskoe", S/7);
+            Console.ReadKey();
+        }
+    }
+}
